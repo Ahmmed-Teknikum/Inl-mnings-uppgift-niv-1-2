a8ea3d84948fe403c9b7330f49e743ad0fcda319
ahka07001 <ahka07001@E-PW05CGQT>
2024-12-10 14:28:12 +0100

    Add project files.

---
+++ Inlämnings uppgift nivå 1 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace inlämningsUppgift_._nivå1_._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ett heltal");
+            string str= Console.ReadLine();
+            int tal= int.Parse(str);
+            while(tal !=0)
+            {
+                Console.WriteLine("Fel, gissa igen:");
+                str = Console.ReadLine();
+                tal = int.Parse(str);
+            }
+            Console.WriteLine("Grattis! Du gissade rätt!");
+        }
+    }
+}
